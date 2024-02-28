d63e4db2898c4fdd49742454313ddb7bd378f6d2
Gabriel Abílio <150153171+g-abilio@users.noreply.github.com>
2024-02-28 10:53:05 -0300

    real number

---
+++ realnumber/build/methods_/CMakeFiles/methods.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/methods.dir/__/realnumber.cpp.o"
+  "CMakeFiles/methods.dir/__/realnumber.cpp.o.d"
+  "libmethods.a"
+  "libmethods.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/methods.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ realnumber/realnumber.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "methods_/methods.hpp"
+#include "build/real_numberconfig.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    binary number;
+    int memory;
+    double valueondec;
+
+    cout << argv[0] << " Version " << real_number_VERSION_MAJOR << "." << real_number_VERSION_MINOR << endl;
+
+    cout << "\n*** REAL NUMBER: STORAGE AND REPRESENTATION ***\n" << endl;
+
+    cout <<  "Type the floating point precision: ";
+    cin >> memory;
+
+    if (memory != 32 && memory != 64)
+    {
+        error ("Error - precision should be simple or double\n");
+    }
+
+    number.setprecision(memory);
+
+    if (memory == 32)
+    {
+        number.resultin32();
+    }
+    else if (memory == 64)
+    {
+        number.resultin64();
+    }
+
+    return 0;
+}
